ab82fcae28b4774ab7d8ad344ca4326fb6ef3da5
Paoli99 <31941327+Paoli99@users.noreply.github.com>
2022-05-12 15:22:54 -0400

    update webserver

--- website.tf
+++ website.tf
@@ -1,156 +1,161 @@
+#Define the provider
 provider "aws" {
-    region = "us-east-1"    
-    access_key = "AKIAYMTN3PCU22QNCXIJ"
-    secret_key = "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"
+    region = "us-west-1"    
+    access_key = "AKIAYMTN3PCUYPWIQ3TP"
+    secret_key = "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"
     //version = "= 2.17.0"
 }
 
-/*terraform {
-      required_providers {
-         aws = {
-         source = "hashicorp/aws"
-         version = "= 3.74.2"
-        }
-     }
-  }
-*/
-
-
-
+#Create a virtual network
 resource "aws_vpc" "my_website_vpc" {
-  cidr_block = "10.16.0.0/16"
-  tags = {
-    "Name" = "my_website_vpc"
-  }
-  assign_generated_ipv6_cidr_block = true 
-  enable_dns_hostnames=true
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "website_vpc"
+    }
 }
 
-resource "aws_subnet" "website_subnet" {
-  tags = {
-        Name = "website_subnet"
+#Create your application segment
+resource "aws_subnet" "my_app-subnet" {
+    tags = {
+        Name = "APP_Subnet"
     }
     vpc_id = aws_vpc.my_website_vpc.id
-    cidr_block = "10.16.0.0/20"
+    cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = true
     depends_on= [aws_vpc.my_website_vpc]
-
+    
 }
 
-resource "aws_route_table" "website_rt" {
-  vpc_id = aws_vpc.my_website_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.website_igw.id
-  }
-
-  tags = {
-    Name = "website_rt"
-  }
+#Define routing table
+resource "aws_route_table" "my_route-table" {
+    tags = {
+        Name = "MY_Route_table"
+       
+    }
+     vpc_id = aws_vpc.my_website_vpc.id
 }
 
-resource "aws_route_table_association" "website_rt_association" {
-  subnet_id = aws_subnet.website_subnet.id 
-  route_table_id = aws_route_table.website_rt.id
+#Associate subnet with routing table
+resource "aws_route_table_association" "App_Route_Association" {
+  subnet_id      = aws_subnet.my_app-subnet.id 
+  route_table_id = aws_route_table.my_route-table.id
 }
 
-resource "aws_internet_gateway" "website_igw" {
-  vpc_id = aws_vpc.my_website_vpc.id
 
-  tags = {
-    Name = "website_igw"
-  }
+#Create internet gateway for servers to be connected to internet
+resource "aws_internet_gateway" "my_IG" {
+    tags = {
+        Name = "MY_IGW"  
+    }
+     vpc_id = aws_vpc.my_website_vpc.id
+     depends_on = [aws_vpc.my_website_vpc]
 }
 
-resource "aws_security_group" "website_sg" {
-  name = "website_sg"
-  description = "Allow web inbound traffic"
-  vpc_id = aws_vpc.my_website_vpc.id 
-  ingress {
-    description      = "http traffic"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
- 
-  tags = {
-    Name = "website_sg"
-  }
+#Add default route in routing table to point to Internet Gateway
+resource "aws_route" "default_route" {
+  route_table_id = aws_route_table.my_route-table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.my_IG.id
 }
 
-resource "tls_private_key" "web-key" {
+#Create a security group
+resource "aws_security_group" "App_SG" {
+    name = "App_SG"
+    description = "Allow Web inbound traffic"
+    vpc_id = aws_vpc.my_website_vpc.id
+    ingress  {
+        protocol = "tcp"
+        from_port = 80
+        to_port  = 80
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress  {
+        protocol = "tcp"
+        from_port = 22
+        to_port  = 22
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress  {
+        protocol = "-1"
+        from_port = 0
+        to_port  = 0
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+#Create a private key which can be used to login to the webserver
+resource "tls_private_key" "Web-Key" {
   algorithm = "RSA"
 }
 
-resource "aws_key_pair" "web-instance-key" {
-  key_name = "web-key"
-  public_key = tls_private_key.web-key.public_key_openssh 
+#Save public key attributes from the generated key
+resource "aws_key_pair" "App-Instance-Key" {
+  key_name   = "Web-key"
+  public_key = tls_private_key.Web-Key.public_key_openssh
 }
 
-resource "local_file" "web-key" {
-  content = tls_private_key.web-key.private_key_pem
-  filename = "web-key.pem"
+#Save the key to your local system
+resource "local_file" "Web-Key" {
+    content     = tls_private_key.Web-Key.private_key_pem 
+    filename = "Web-Key.pem"
 }
 
-resource "aws_instance" "web-instance" {
-  ami = "ami-0022f774911c1d690"
-  instance_type = "t2.micro"
+#Create your webserver instance
+resource "aws_instance" "Web" {
+    ami = "ami-02541b8af977f6cdd"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "WebServer1"
+    }
+    count =1
+    subnet_id = aws_subnet.my_app-subnet.id 
+    key_name = "Web-key"
+    security_groups = [aws_security_group.App_SG.id]
+
+    provisioner "remote-exec" {
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        private_key = tls_private_key.Web-Key.private_key_pem
+        host = aws_instance.Web[0].public_ip
+    }    
+    inline = [
+       "sudo yum install httpd  php git -y",
+       "sudo systemctl restart httpd",
+       "sudo systemctl enable httpd",
+    ]
+  }
+
+}
+
+#Create a block volume for data persistence
+resource "aws_ebs_volume" "myebs1" {
+  availability_zone = aws_instance.Web[0].availability_zone
+  size              = 1
   tags = {
-    "Name" = "WebServer"
-  }
-  count = 1 
-  subnet_id = aws_subnet.website_subnet.id
-  key_name = "web-key"
-  security_groups = [aws_security_group.website_sg.id]
-
-  provisioner "remote-exec" {
-  connection {
-      type = "ssh"
-      user = "ec2-user"
-      agent = true 
-      private_key = tls_private_key.web-key.private_key_pem
-      host = aws_instance.web-instance[0].public_ip
-  }
-  inline = [
-        "sudo yum install httpd  php git -y",
-        "sudo systemctl restart httpd",
-        "sudo systemctl enable httpd",
-  ]
-  }
-} 
-
-resource "aws_ebs_volume" "website_ebs" {
-  availability_zone = aws_instance.web-instance[0].availability_zone
-  size = 1
-  tags = {
-    "Name" = "website_ebs"
+    Name = "ebsvol"
   }
 }
 
-resource "aws_volume_attachment" "attatch_ebs" {
-  depends_on = [aws_ebs_volume.website_ebs]
+#Attach the volume to your instance
+resource "aws_volume_attachment" "attach_ebs" {
+  depends_on = [aws_ebs_volume.myebs1]
   device_name = "/dev/sdh"
-  volume_id = aws_ebs_volume.website_ebs.id
-  instance_id = aws_instance.web-instance[0].id 
-  force_detach = true 
+  volume_id   = aws_ebs_volume.myebs1.id
+  instance_id = aws_instance.Web[0].id
+  force_detach = true
 }
 
+#Mount the volume to your instance
 resource "null_resource" "nullmount" {
-  depends_on = [aws_volume_attachment.attatch_ebs]
-  connection {
-      type = "ssh"
-      user = "ec2-user"
-      private_key = tls_private_key.web-key.private_key_pem
-      host = aws_instance.web-instance[0].public_ip
+  depends_on = [aws_volume_attachment.attach_ebs]
+    connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = tls_private_key.Web-Key.private_key_pem
+    host     = aws_instance.Web[0].public_ip
   }
   provisioner "remote-exec" {
     inline = [
@@ -162,52 +167,58 @@ resource "null_resource" "nullmount" {
   }
 }
 
+#Define S3 ID
 locals {
-  s3_origin_id = "s3-origin"
+ s3_origin_id = "s3-origin"
 }
 
-resource "aws_s3_bucket" "website-bucket" {
-  bucket = "website-bucket"
-  acl = "public-read-write"
-  //region = "us-east-1a"
+#Create a bucket to upload your static data like images
+resource "aws_s3_bucket" "mocknetserv" {
+  bucket = "mocknetserv"
+  acl    = "public-read-write"
+  //region = "us-east-1"
 
   versioning {
     enabled = true
   }
 
   tags = {
-    Name = "website-bucket"
+    Name = "mocknetserv"
     Environment = "PROD_UPB"
   }
 
-  provisioner "local-exec" {
-      command = "git clone https://github.com/Paoli99/certi-website.git web-server-image"
-  }
+ provisioner "local-exec" {
+    command = "git clone https://github.com/Paoli99/certi-website.git web-server-image"
+ }  
+
 }
 
+#Allow public access to the bucket
 resource "aws_s3_bucket_public_access_block" "public_storage" {
-  depends_on = [aws_s3_bucket.website-bucket]
-  bucket = "website-bucket"
-  block_public_acls = false
-  block_public_policy = false 
+ depends_on = [aws_s3_bucket.mocknetserv]
+ bucket = "mocknetserv"
+ block_public_acls = false
+ block_public_policy = false
 }
 
-/* resource "aws_s3_bucket_object" "website-object" {
-  depends_on = [aws_s3_bucket.website-bucket]
-  bucket = "website-bucket"
-  acl = "public-read-write"
-  key = "HIMYM.jpg"
-  source = "web-server-image/HIMYM.jpg"
-} */
+#Upload your data to S3 bucket
+ resource "aws_s3_bucket_object" "Object1" {
+  depends_on = [aws_s3_bucket.mocknetserv]
+  bucket = "mocknetserv"
+  acl    = "public-read-write"
+  key = "HIMYM.JPG"
+  source = "HIMYM.JPG"
+} 
 
-resource "aws_cloudfront_distribution" "web-cloudfront" {
-  depends_on = [aws_s3_bucket_object.website-object]
-  origin{ 
-      domain_name = aws_s3_bucket.website-bucket.bucket
-      origin_id = local.s3_origin_id
-  }
-  enabled = true 
-  default_cache_behavior {
+#Create a Cloudfront distribution for CDN
+resource "aws_cloudfront_distribution" "tera-cloufront1" {
+    //depends_on = [ aws_s3_bucket_object.Object1]
+    origin {
+        domain_name = aws_s3_bucket.mocknetserv.bucket_regional_domain_name
+        origin_id = local.s3_origin_id
+    }   
+    enabled = true
+      default_cache_behavior {
         allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
         cached_methods = ["GET", "HEAD"]
         target_origin_id = local.s3_origin_id
@@ -237,18 +248,19 @@ resource "aws_cloudfront_distribution" "web-cloudfront" {
     } 
 }
 
-/* resource "null_resource" "Write_Image" {
-    depends_on = [aws_cloudfront_distribution.web-cloudfront]
+#Update the CDN image URL to your webserver code.
+resource "null_resource" "Write_Image" {
+    depends_on = [aws_cloudfront_distribution.tera-cloufront1]
     connection {
     type     = "ssh"
     user     = "ec2-user"
-    private_key = tls_private_key.web-key.private_key_pem
-    host     = aws_instance.web-instance[0].public_ip
+    private_key = tls_private_key.Web-Key.private_key_pem
+    host     = aws_instance.Web[0].public_ip
      }
   provisioner "remote-exec" {
         inline = [
             "sudo su << EOF",
-                    "echo \"<img src='http://${aws_cloudfront_distribution.web-cloudfront.domain_name}/${aws_s3_bucket_object.website-object.key}' width='300' height='380'>\" >>/var/www/html/index.html",
+                    "echo \"<img src='http://${aws_cloudfront_distribution.tera-cloufront1.domain_name}/${aws_s3_bucket_object.Object1.key}' width='300' height='380'>\" >>/var/www/html/index.html",
                     "echo \"</body>\" >>/var/www/html/index.html",
                     "echo \"</html>\" >>/var/www/html/index.html",
                     "EOF",    
@@ -256,18 +268,19 @@ resource "aws_cloudfront_distribution" "web-cloudfront" {
   }
 
 }
- */
 
+#success message and storing the result in a file
 resource "null_resource" "result" {
-    //depends_on = [null_resource.nullmount]
+    depends_on = [null_resource.nullmount]
     provisioner "local-exec" {
-    command = "echo The website has been deployed successfully and >> result.txt  && echo the IP of the website is  ${aws_instance.web-instance[0].public_ip} >>result.txt"
+    command = "echo The website has been deployed successfully and >> result.txt  && echo the IP of the website is  ${aws_instance.Web[0].public_ip} >>result.txt"
   }
 }
 
+#Test the application
 resource "null_resource" "running_the_website" {
-    //depends_on = [null_resource.Write_Image]
+    depends_on = [null_resource.Write_Image]
     provisioner "local-exec" {
-    command = "start chrome ${aws_instance.web-instance[0].public_ip}"
+    command = "start chrome ${aws_instance.Web[0].public_ip}"
   }
 }
